d3509ed393db16c994ef20f686c2bdf94a213bc0
hinduja2 <115637140+hinduja2@users.noreply.github.com>
2022-10-18 13:00:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Content from './Components/Content/content';
+import Footer from './Components/Footer/footer';
+import Header from './Components/Header/header';
+
+
+
+
+function App() {
+  return (
+    <>
+
+    <Content />
+    
+    </>
+    
+  );
+}
+
+export default App;
